8e19cea64af30bcaa75e8178a0fb1c604ed6e6bc
dogebi2 <peter.xu@xenera.jp>
2022-12-29 15:53:39 +0800

    Initial Commit

---
+++ DarkMode_Swiftui/DarkMode_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkMode_SwiftuiApp.swift
+//  DarkMode_Swiftui
+//
+//  Created by rainxlh on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct DarkMode_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
